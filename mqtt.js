d135035f54458713de41c91daefe7b128f94db2f
mayoushang <59989586+mayoushang@users.noreply.github.com>
2020-06-19 15:49:52 +0800

    Add files via upload

--- mqtt.js
+++ mqtt.js
@@ -2,7 +2,7 @@ var instanceId = mq.instanceid;//实例 ID，购买后从控制台获取
 var host = mq.host;// 设置当前用户的接入点域名，接入点获取方法请参考接入准备章节文档，先在控制台创建实例
 var port = window.location.protocol=="https:" ? 443 : 80;//WebSocket 协议服务端口，如果是走 HTTPS，设置443端口
 var topic = mq.barrage_topic;//需要操作的 Topic,第一级父级 topic 需要在控制台申请
-var useTLS = false;//是否走加密 HTTPS，如果走 HTTPS，设置为 true
+var useTLS = window.location.protocol=="https:" ? true : false;//是否走加密 HTTPS，如果走 HTTPS，设置为 true
 var accessKey = mq.AccessKeyId;//账号的 AccessKey，在阿里云控制台查看
 var secretKey = mq.AccessKeySecret;
 var cleansession = true;
